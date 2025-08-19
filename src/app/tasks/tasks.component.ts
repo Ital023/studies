a3840708008b6391bb2be75b3fafe23e86e2c093
Ital023 <contadoitalo911@gmail.com>
2025-08-18 21:48:40 -0300

    new task

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,19 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { DUMMY_TASKS } from '../dummy-tasks';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input({required: true}) name!: string
-  @Input({required: true}) userId!: string
+  @Input({required: true}) name!: string;
+  @Input({required: true}) userId!: string;
+  isAddingTask = false;
 
-  tasks = DUMMY_TASKS
+  tasks = DUMMY_TASKS;
 
   get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userId)
@@ -21,6 +23,10 @@ export class TasksComponent {
 
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true
   }
 
 }
